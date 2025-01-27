38158027f645264f8a9f1d77d4db4e6f99653a6a
Kristian Orozco <kristian.orozco@infracommerce.lat>
2025-01-26 21:25:23 -0500

    feat: agregar columna de estado a las tablas de contenido, menús, elementos de menú, contenidos y widgets

--- database/migrations/2024_12_05_045323_add_status_to_content_types_table.php
+++ database/migrations/2024_12_05_045323_add_status_to_content_types_table.php
@@ -11,34 +11,57 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('content_types', function (Blueprint $table) {
-            $table->string('status')->default('active')->after('schema');
-        });
+        if (!Schema::hasColumn('content_types', 'status')) {
+            Schema::table('content_types', function (Blueprint $table) {
+                $table->string('status')->default('active')->after('schema');
+            });
+        }
 
-        Schema::table('menus', function (Blueprint $table) {
-            $table->string('status')->default('active');
-        });
+        if (!Schema::hasColumn('menus', 'status')) {
+            Schema::table('menus', function (Blueprint $table) {
+                $table->string('status')->default('active');
+            });
+        }
 
-        Schema::table('menu_items', function (Blueprint $table) {
-            $table->string('status')->default('active');
-        });
+        if (!Schema::hasColumn('menu_items', 'status')) {
+            Schema::table('menu_items', function (Blueprint $table) {
+                $table->string('status')->default('active');
+            });
+        }
 
-        Schema::table('contents', function (Blueprint $table) {
-            $table->string('status')->default('active');
-        });
+        if (!Schema::hasColumn('contents', 'status')) {
+            Schema::table('contents', function (Blueprint $table) {
+                $table->string('status')->default('active');
+            });
+        }
 
-        Schema::table('widgets', function (Blueprint $table) {
-            $table->string('status')->default('active');
-        });
+        if (!Schema::hasColumn('widgets', 'status')) {
+            Schema::table('widgets', function (Blueprint $table) {
+                $table->string('status')->default('active');
+            });
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('content_types', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
+        });
+
+        Schema::table('menus', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('menu_items', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('contents', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('widgets', function (Blueprint $table) {
+            $table->dropColumn('status');
         });
     }
 };
