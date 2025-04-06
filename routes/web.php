6edc3afa9a7dc5123c03d137cb1616d47c26311d
Kristian Orozco <ing.korozco@outlook.com>
2025-04-06 16:53:59 -0500

    feat: agregar ruta para publicar el visor de logs con verificación de acceso

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,5 +12,20 @@ Route::get('/tracking')->name('tracking');
 //using SupportController the method verifyStatus to get the status of the ticket
 Route::get('/support/{id}/verify')->name('support.verify');
 Route::get('/support')->name('support.store');
+
+Route::get('/publish-log-viewer', function () {
+    // Verifica si el usuario está autenticado y es administrador
+    if (!Auth::check() || Auth::user()->role !== 'superadmin') {
+        abort(403, 'Acceso no autorizado');
+    }
+
+    // Ejecuta el comando Artisan
+    try {
+        Artisan::call('log-viewer:publish');
+        return 'Log viewer publicado correctamente: ' . Artisan::output();
+    } catch (\Exception $e) {
+        return 'Error al ejecutar el comando: ' . $e->getMessage();
+    }
+})->middleware(['auth'])->name('publish.log-viewer');
 
 require __DIR__.'/auth.php';
