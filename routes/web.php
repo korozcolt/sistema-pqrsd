dfbc9b187e0e6b9ec216469fbbd5a3073c9f924b
Kristian Orozco <kristian.orozco@infracommerce.lat>
2025-11-02 19:37:24 -0500

    Add home route to display welcome view

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
 
 Route::get('/tracking')->name('tracking');
